9951849e450f1e536e687468033d8ca468e56a50
toxinf <49251275+toxinf@users.noreply.github.com>
2024-02-10 05:41:37 +0600

    Annotation is deprecated

--- test-for-candidates/src/Controller/PriceCalculatorController.php
+++ test-for-candidates/src/Controller/PriceCalculatorController.php
@@ -7,7 +7,7 @@ use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use App\Dto\CalculatePriceRequest;
 use App\Service\PriceCalculatorService;
 use Symfony\Component\Serializer\SerializerInterface;
